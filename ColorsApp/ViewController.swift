b2e79250d77a970748f3f27302345d0c6a9a797f
msnbek <mahmutsenbek@icloud.com>
2023-01-05 16:52:15 +0300

    Initial Commit

---
+++ ColorsApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ColorsApp
+//
+//  Created by Mahmut Senbek on 5.01.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
